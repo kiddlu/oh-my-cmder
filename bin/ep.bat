976fc83150addb1603bfa2db46cfc9d77260c407
kiddlu <kidd.dawny.lu@gmail.com>
2020-08-25 16:39:42 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- bin/ep.bat
+++ bin/ep.bat
@@ -1,3 +1,14 @@
 @echo off
 
-explorer.exe %*
+if '%1'=='??' (
+	for /f "delims=" %%i in (' dir /b /s /a:-D ^| peco ') do (explorer.exe %%i)
+	cls
+) else if '%1'=='?' (
+	for /f "delims=" %%i in (' dir /b /a:-D ^| peco ') do (explorer.exe %%i)
+	cls
+) else (
+	explorer.exe %*
+)
+
+
+
--- bin/epp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-for /f "delims=" %%i in (' dir /b /a:-D ^| peco ') do (explorer.exe %%i)
-cls
