ad9aee2d485dec368b821c82394c201da8629c6b
kiddlu <kidd.dawny.lu@gmail.com>
2021-05-26 10:32:55 +0800

    update Vendor
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- vendor/scrcpy/adb-scrcpy.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-
-set SCRCPY_ROOT=%CMDER_ROOT%\vendor\scrcpy
-set OLDPWD="%cd%"
-
-cd %SCRCPY_ROOT%
-%SCRCPY_ROOT%/scrcpy.exe
-cd %OLDPWD%
--- vendor/scrcpy/install.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-set SCRCPY_ROOT=%CMDER_ROOT%\vendor\scrcpy
-set SCRCPY_SETUP=%SCRCPY_ROOT%\scrcpy.zip
-
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "((New-Object System.Net.WebClient).DownloadFile('https://github.com/Genymobile/scrcpy/releases/download/v1.6/scrcpy-win32-v1.6.zip','%SCRCPY_SETUP%'))"
-
-%~dp0\..\busybox\busybox.exe unzip %SCRCPY_ROOT%\scrcpy.zip -d %SCRCPY_ROOT%
-%~dp0\..\busybox\busybox.exe rm %SCRCPY_ROOT%\scrcpy.zip
