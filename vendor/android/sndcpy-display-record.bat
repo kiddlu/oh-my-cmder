0be311dd81194af574c301c436df833141e74071
kiddlu <kidd.dawny.lu@gmail.com>
2021-06-05 22:28:47 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- vendor/android/sndcpy-display-record.bat
+++ vendor/android/sndcpy-display-record.bat
@@ -4,6 +4,10 @@ if not defined VLC set VLC="C:\Program Files\VideoLAN\VLC\vlc.exe"
 if not defined SNDCPY_APK set SNDCPY_APK=sndcpy.apk
 if not defined SNDCPY_PORT set SNDCPY_PORT=28200
 
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%
+set timetag=%ldt%%time:~0,2%%time:~3,2%%time:~6,2%
+
 if not "%1"=="" (
     set serial=-s %1
     echo Waiting for device %1...
@@ -22,7 +26,8 @@ if not "%1"=="" (
 echo Press Enter once audio capture is authorized on the device to start playing...
 pause >nul
 echo Playing audio...
-%VLC% --demux rawaud --network-caching=50 tcp://localhost:%SNDCPY_PORT% vlc://quit
+::%VLC% --demux rawaud --network-caching=50 tcp://localhost:%SNDCPY_PORT% --sout "#transcode{acodec=s16l,channels=2,samplerate=48000}:duplicate{dst=std{access=file,mux=wav,dst=record_%timetag%.wav},dst=display}" vlc://quit
+%VLC% --demux rawaud --network-caching=50 tcp://localhost:%SNDCPY_PORT% --sout "#transcode{acodec=flac}:duplicate{dst=std{access=file,mux=raw,dst=record_%timetag%.flac},dst=display}" vlc://quit
 goto :EOF
 
 :error
